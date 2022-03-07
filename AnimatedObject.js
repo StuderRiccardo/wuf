78e7533963fad4895af4d33cd9c4853c5f9bfd49
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2022-03-07 12:27:37 +0000

    parte3

--- AnimatedObject.js
+++ AnimatedObject.js
@@ -1,13 +1,15 @@
 export default class AnimatedObject {
   speedX = 0;
   speedY = 0;
-  width = 0;
-  height = 00;
-  x = 0;
-  y = 0;
+  width = 60;
+  height = 60;
+  x = 10;
+  y = 120;
   imageList = [];
   contaFrame = 0;
   actualFrame = 0;
+  tryX = 0;
+  tryY = 0;
 
   constructor(imageUrlList, width, height, x, y) {
     this.loadImages(imageUrlList);
@@ -17,9 +19,24 @@ export default class AnimatedObject {
     this.y = y;
   }
 
-  update() {
-    this.y = this.y + this.speedY;
-    this.x = this.x + this.speedX;
+  update(obstacleVector) {
+    this.tryY = this.y + this.speedY;
+    this.tryX = this.x + this.speedX;
+
+    //Prima di spostarmi realmente verifico che non ci siano collisioni
+    let collision = false;
+
+    for (let object of obstacleVector) {
+      if (collision == false) {
+        collision = this.crashWith(object);
+      }
+    }
+
+    if (!collision) {
+      this.x = this.tryX;
+      this.y = this.tryY;
+    }
+
     if (this.speedX != 0 || this.speedY != 0) this.contaFrame++;
     if (this.contaFrame == 3) {
       this.contaFrame = 0;
@@ -28,7 +45,37 @@ export default class AnimatedObject {
     }
   }
 
-    loadImages(imageUrlList) {
+  crashWith(otherobj) {
+    var myleft = this.tryX;
+    var myright = this.tryX + this.width;
+    var mytop = this.tryY;
+    var mybottom = this.tryY + this.height;
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + otherobj.width;
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + otherobj.height;
+    var crash = true;
+
+    //Collisioni con il bordo
+    if (mytop < 0 || mybottom > 270 || myleft < 0 || myright > 480) {
+      return true;
+    }
+
+    //NON HO COLLISIONI
+    if (
+      mybottom < othertop ||
+      mytop > otherbottom ||
+      myright < otherleft ||
+      myleft > otherright
+    ) {
+      return false;
+    } //HO COLLISIONI
+    else {
+      return true;
+    }
+  }
+
+  loadImages(imageUrlList) {
     for (let imgPath of imageUrlList) {
       var img = new Image(this.width, this.height);
       img.src = imgPath;
@@ -36,4 +83,8 @@ export default class AnimatedObject {
     }
     this.image = this.imageList[this.actualFrame];
   }
+
+  /*jump() {
+    this.gravitySpeed = -this.jumpForce;
+  }*/
 }
