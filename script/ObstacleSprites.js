968ddf92f4019c16c16b06e8547e8b34b80d4b7f
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2022-03-28 10:52:08 +0000

    parte_della_mappa

---
+++ script/ObstacleSprites.js
@@ -0,0 +1,6 @@
+export default class ObstacleSprites {
+    static bush = ["https://i.ibb.co/CPdHYdB/Bush-1.png"];
+    static crate = ["https://i.ibb.co/GMgf32L/Crate.png"];
+  }
+  
+  
---
+++ script/canvas.js
@@ -0,0 +1,18 @@
+/*export default class canvas {
+    constructor() {
+      this.canvas = document.getElementById("canvas");
+      this.canvas.width = 640;
+      this.canvas.height = 640;
+      this.context = this.canvas.getContext("2d");
+      this.interval = setInterval(this.updateGameArea, 20); //ogni 20 ms chiamo il metodo updateGameArea
+    }
+  
+    clear = () => {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    };
+  
+    updateGameArea = () => {
+      this.clear();
+    };
+  }
+  */
